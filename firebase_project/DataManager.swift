928fe0d3016c6ca98b2ee0c762da15553ed1dda4
kaizen2112 <tasrif701@gmail.com>
2024-11-27 18:44:46 +0600

    Data Manager and New Dog view added

---
+++ firebase_project/DataManager.swift
@@ -0,0 +1,45 @@
+
+import SwiftUI
+import Firebase
+import FirebaseFirestore
+
+class DataManager: ObservableObject {
+    @Published var dogs: [Dog] = []
+
+    init() {
+        fetchDogs()
+    }
+    
+    func fetchDogs() {
+        dogs.removeAll()
+        let db = Firestore.firestore()
+        let ref = db.collection("Dogs")
+        
+        ref.getDocuments { snapshot, error in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+            
+            if let snapshot = snapshot {
+                for document in snapshot.documents {
+                    let data = document.data()
+                    let id = data["id"] as? String ?? ""
+                    let breed = data["breed"] as? String ?? ""
+                    let dog = Dog(id: id, breed: breed)
+                    self.dogs.append(dog)
+                }
+            }
+        }
+    }
+    
+    func addDog(dogBread: String){
+        let db = Firestore.firestore()
+        let ref = db.collection("Dogs").document(dogBread)
+        ref.setData(["breed": dogBread, "id": 10]) { error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
