ae33e28f8844edd1951d9f2500d4242d10c37eb1
Sajib1472 <132594756+Sajib1472@users.noreply.github.com>
2024-11-27 18:09:19 +0600

    contentView file updated

--- firebase_project/ContentView.swift
+++ firebase_project/ContentView.swift
@@ -16,7 +16,87 @@ struct ContentView: View {
         }
     }
     
-    
+    var content: some View {
+            ZStack {
+                Color.black
+                
+                RoundedRectangle(cornerRadius: 30, style: .continuous)
+                    .foregroundStyle(.linearGradient(colors: [.pink, .red], startPoint: .topLeading, endPoint: .bottomTrailing))
+                    .frame(width: 1000, height: 400)
+                    .rotationEffect(.degrees(135))
+                    .offset(y: -350)
+                
+                VStack(spacing: 20) {
+                    Text("Welcome")
+                        .foregroundColor(.white)
+                        .font(.system(size: 40, weight: .bold, design: .rounded))
+                        .offset(x: -100, y: -100)
+                    
+                    TextField("Email", text: $email)
+                        .foregroundColor(.white)
+                        .textFieldStyle(.plain)
+                        .placeholder(when: email.isEmpty) {
+                            Text("Email")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                    Rectangle()
+                        .frame(width: 350, height: 1)
+                        .foregroundColor(.white)
+
+                    SecureField("Password", text: $password)
+                        .foregroundColor(.white)
+                        .textFieldStyle(.plain)
+                        .placeholder(when: password.isEmpty) {
+                            Text("Password")
+                        }
+
+                    Rectangle()
+                        .foregroundColor(.white)
+                        .bold()
+                        .frame(width: 350, height: 1)
+                        .foregroundColor(.white)
+
+                    Button {
+                        register()
+                    } label: {
+                        Text("Sign up")
+                            .bold()
+                            .frame(width: 200, height: 40)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                    .fill(
+                                        LinearGradient(
+                                            colors: [.pink, .red],
+                                            startPoint: .top,
+                                            endPoint: .bottomTrailing))
+                            )
+                            .foregroundColor(.white)
+                    }
+                    .padding(.top)
+                    .offset(y: 100)
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Already have an account? Login")
+                            .bold()
+                            .foregroundColor(.white)
+                    }
+                    .padding(.top)
+                    .offset(y: 110)
+                    
+                }
+                .frame(width: 350)
+                .onAppear {
+                    Auth.auth().addStateDidChangeListener { auth, user in
+                        if user != nil {
+                            userIsLoggedIn.toggle()                    }
+                    }
+                }
+            }
+            .ignoresSafeArea()
+        }
     
 }
 
